216840dca7a6d1c81ec4fdbec516d98f3ffd2671
sachimugu <sachimugu@outlook.com>
2022-04-06 20:05:56 +0100

    django website

--- DjangoIcuSite/IcuMortality/views.py
+++ DjangoIcuSite/IcuMortality/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+import joblib
 # Create your views here.
+
+def Home(request):
+
+    if request.method == "POST":
+
+        model=joblib.load('job_rf.pkl')
+        data= []
+        data.append(request.POST["index0"])
+        data.append(request.POST["index1"])
+        data.append(request.POST["index2"])
+        data.append(request.POST["index3"])
+        data.append(request.POST["index4"])
+        data.append(request.POST["index5"])
+        data.append(request.POST["index6"])
+        data.append(request.POST["index7"])
+        data.append(request.POST["index8"])
+        data.append(request.POST["index9"])
+        data.append(request.POST["index10"])
+    
+        prediction=model.predict([data])[0]
+
+        if prediction > 0:
+            context = {'pred_result':'ICU MORTALITY IS HIGH'}
+        else:
+            context = {'pred_result':'ICU MORTALITY IS LOW'}
+    else:
+        context={}
+
+    return render(request, 'IcuMortality/home.html', context=context )
+
